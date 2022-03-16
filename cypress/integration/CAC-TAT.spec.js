734bea2772bdf742c360bc5a76edcde377cac470
Gabriel Stefanini Mattar <vntgast@Gabriel-Stefanini-Mattar.local>
2022-03-16 14:41:47 -0300

    Lesson 2 - exercicio 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,8 +2,21 @@
 
 describe('Central de Atendimento ao Cliente TAT', () => {
 
-    it('Verifica o titulo da aplicação', () => {
+    beforeEach(() => {
         cy.visit('./src/index.html')
+    })
+
+    it('Verifica o titulo da aplicação', () => {
         cy.title().should('be.eq', 'Central de Atendimento ao Cliente TAT')
     })
+
+    it('Preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('#firstName').type('Gabriel')
+        cy.get('#lastName').type('Mattar')
+        cy.get('#email').type('gabriel@email.com')
+        cy.get('#open-text-area').type('Teste Teste Teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
+
 })
